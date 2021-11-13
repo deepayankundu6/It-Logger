b4b97f0f3a8db80d787ea01dec8d325591c1f201
Deepayan Kundu <deepayankundu6@gmail.com>
2021-11-13 19:26:55 +0530

    vfvv

--- src/components/layouts/TabsMenu.js
+++ src/components/layouts/TabsMenu.js
@@ -8,11 +8,11 @@ const TabsMenu = () => {
             <div class="row">
                 <div class="col s12">
                     <ul class="tabs">
-                        <li class="tab col s3"><a href="#test1">Users</a></li>
+                        <li class="tab col s3"><a href="#test1">Technicians</a></li>
                         <li class="tab col s3 right"><a class="active" href="#test2">System Logs</a></li>
                     </ul>
                 </div>
-                <div id="test1" class="col s12">Active users list</div>
+                <div id="test1" class="col s12">Active techs list here!!!</div>
                 <div id="test2" class="col s12"><Logs></Logs></div>
             </div>
         </Fragment>
