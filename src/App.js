166be16788a588b0b07ef975073d366435b05afa
Deepayan Kundu <deepayankundu6@gmail.com>
2021-11-13 12:44:12 +0530

    final setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js'
 
 const App = () => {
+
+  useEffect(() => {
+    // Initialises materialise javascript
+    M.AutoInit();
+  });
   return (
     <div className="App">
       <h2> My App is ready for development!!!.</h2>
